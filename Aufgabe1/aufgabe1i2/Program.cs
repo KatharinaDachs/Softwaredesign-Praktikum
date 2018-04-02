5a1fc05f7d49f0fb0deb36d8416a4e59ea07d01b
KatharinaDachs <k.dachs@hotmail.de>
2018-04-03 00:40:03 +0200

    bugfree aufgabe1i2

--- Aufgabe1/aufgabe1i2/Program.cs
+++ Aufgabe1/aufgabe1i2/Program.cs
@@ -9,9 +9,8 @@ namespace aufgabe1i2
         // SUBJECT
         static string[] subjects = { "Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore" };
 
-                // Kopie von Subject Shuffle
-
-        static Random subject=new Random();
+            // Kopie von Subject Shuffle
+        static Random subject = new Random();
         static string[] RandomSubject = subjects.OrderBy(x => subject.Next()).ToArray();
 
         static int subjectindex = 0;
@@ -19,50 +18,64 @@ namespace aufgabe1i2
         //VERB
         static string[] verbs = { "braut", "liebt", "studiert", "hasst", "zaubert", "zerstört" };
 
-                //Kopie von Verb Shuffle
-        static Random verb =new Random();
+            //Kopie von Verb Shuffle
+        static Random verb = new Random();
         static string[] RandomVerb = verbs.OrderBy(x => verb.Next()).ToArray();
         static int verbindex = 0;
 
         // OBJECT
         static string[] objects = { "Zaubertränke", "den Grimm", "Lupin", "Hogwards", "die Karte des Rumtreibers", "Dementoren" };
 
-                // Kopie von Object Shuffle
-        static Random objectt =new Random();
+            // Kopie von Object Shuffle
+        static Random objectt = new Random();
         static string[] RandomObject = objects.OrderBy(x => objectt.Next()).ToArray();
         static int objectindex = 0;
 
         // SÄTZE
-        static string[] sentences = {};
+        /*static string[] sentences = { };*/
+        string[] zeile = new string [RandomSubject.Length];
+
+        static string Sub;
+        static string Verb;
+        static string Obj;
 
         static void Main(string[] args)
         {
             // Verse werden geschrieben
-            for(int i=0; i<RandomSubject.Length; i++){
-                sentences[i]= GetVerse();
+            string[] zeile = new string[RandomSubject.Length];
+            for (int i = 0; i < RandomSubject.Length; i++)
+            {
+                GetVerse();
+                zeile[i] = Sub + " " + Verb + " " + Obj;
+                
+                
             }
-            for (int i = 0; i< RandomSubject.Length; i++){
-            Console.WriteLine(sentences[i]);
+            for (int i = 0; i < RandomSubject.Length; i++)
+            {
+                Console.WriteLine(zeile[i]);
+            
             }
         }
-        public static string GetVerse(){
+
+        
+        public static void GetVerse()
+        {
             //random subject
-                // Random wort wird vom Random Array, der oben erzeugt wurde,ausgewählt. Hierbei wird mit dem Wort im Index 0 begonnen und bei erneutem 
-                // aufrufen Idnex 1, 2, 3, etc eingesetzt. 
+            // Random wort wird vom Random Array, der oben erzeugt wurde,ausgewählt. Hierbei wird mit dem Wort im Index 0 begonnen und bei erneutem 
+            // aufrufen Index 1, 2, 3, etc eingesetzt. 
             string sub = RandomSubject[subjectindex];
             subjectindex += 1;
+            Sub = sub;
 
             // random verb
             string ver = RandomVerb[verbindex];
             verbindex += 1;
+            Verb = ver;
 
             //random object
             string obj = RandomObject[objectindex];
             objectindex += 1;
-
-            // Verszeile wird zusammengefügt
-            string zeile = sub + " " + ver + " " + obj;
-            return zeile;
+            Obj = obj;
 
         }
     }
