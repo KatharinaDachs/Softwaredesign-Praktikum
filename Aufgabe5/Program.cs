9b15d6c587456e3e9d0ed70abda81b0d8cefd528
KatharinaDachs <k.dachs@hotmail.de>
2018-05-26 16:43:42 +0200

    aufgabe5 added

---
+++ Aufgabe5/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Aufgabe5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        public class Person
+        {
+            string name;
+            int age;
+        }
+
+        public class Teilnehmer : Person
+        {
+            int registration_number;
+
+            string[] kurse;
+
+            }
+        }
+        public class Dozent : Person
+        {
+            float room;
+            DateTime office_hours;
+
+            string[] kurse;
+
+            }
+        }
+        public class Course
+        {
+            string title;
+            DateTime course_hours;
+            float room;
+            string dozent;
+            string[] teilnehmer;
+        {
+    }
+}
