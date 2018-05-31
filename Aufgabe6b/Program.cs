b71222213a9240bca73446834b775cfef703d631
KatharinaDachs <k.dachs@hotmail.de>
2018-05-31 09:42:26 +0200

    Aufgabe 6b added

---
+++ Aufgabe6b/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DesPatternDecorator
+{
+
+    
+    public class Spielfigur
+    {
+        public virtual void Drohe()
+        {
+        }
+    }
+    public class Monster : Spielfigur
+    {
+        public override void Drohe()
+        {
+            // Calling the base class GetInfo method:
+           // base.Drohe();
+            Console.Write("GRRR");
+        }
+    }
+
+    class Held : Spielfigur
+    {
+        public override void Drohe()
+        {
+            // Calling the base class GetInfo method:
+            //base.Drohe();
+            Console.Write("Weiche zurück");
+        }
+    }
+
+    //Decorator
+     public class Decorator : Spielfigur
+    {
+        private Spielfigur _original;
+        public Decorator(Spielfigur original)
+        {
+            _original = original; 
+        }
+        public override void Drohe()
+        {
+            _original.Drohe();
+        }
+    }
+
+    class ErkaelteteFigur : Decorator
+    {
+        public ErkaelteteFigur(Spielfigur original)
+        
+           :base (original){}
+        
+        public override void Drohe()
+        {
+            base.Drohe();
+            Console.Write(" Hust!");
+        }
+    }
+
+    class HeisereFigur : Decorator
+    {
+        public HeisereFigur(Spielfigur original)
+        :base (original){}
+
+        public override void Drohe()
+        {
+            Console.Write("Räusper....");
+            base.Drohe();
+        }
+    }
+
+    class TestClass
+    {
+        static void Main()
+        {
+
+            List<Spielfigur> figuren = new List<Spielfigur>();
+
+            figuren.Add(new Monster());
+            figuren.Add(new Held());
+            figuren.Add(new ErkaelteteFigur(new Held()));
+            figuren.Add(new HeisereFigur(new Held()));
+
+            foreach(var spielfigur in figuren)
+            {
+                spielfigur.Drohe();
+                Console.WriteLine();
+            }
+        }
+    }
+}
